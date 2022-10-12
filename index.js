217d413109f46d3b190c95d0171d3a5a66bb3655
Mae Schreck <maedevlin47@gmail.comm>
2022-10-11 21:54:39 -0400

    commit tree 5cba8418fe2ccb660b7747fa67db144d60c8cb33

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    //console.log(`This Saturday, I want to ${activity}!`);
+    console.log(activity);
+    return `This Saturday, I want to ${activity}!`;
+};
+
+saturdayFun();
+
+function mondayWork(plan='go to the office') {
+    console.log(plan);
+    return `This Monday, I will ${plan}.`;
+};
+
+mondayWork();
+
+function* wrapAdjective(inner) {
+    console.log(wrapAdjective);
+};
+
+function wrapAdjective(quality="special"){
+    console.log(quality);
+        return `("You are ${quality}")`
+};
